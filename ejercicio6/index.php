35a3e965c46b54d0d63f62a15bf88471957928b5
MIGUEL\angelmiguel <miguel.pozo@epn.edu.ec>
2019-04-23 11:47:05 -0500

    Ejercicio 6 vocales

--- ejercicio6/index.php
+++ ejercicio6/index.php
@@ -10,4 +10,26 @@ aceituno
 acuífero
 http://buscapalabras.com.ar/palabras-con-las-cinco-vocales.html
  */
+
+$palabra  =  "murcielagoas" ;
+
+
+$longitud  =  strlen ( $palabra );
+
+$Letra_a = substr_count($palabra,"a");
+$Letra_e = substr_count($palabra,"e");
+$Letra_i = substr_count($palabra,"i");
+$Letra_o = substr_count($palabra,"o");
+$Letra_u = substr_count($palabra,"u");
+
+if($Letra_a == 1 and $Letra_e == 1 and $Letra_i == 1
+   and $Letra_o == 1 and $Letra_u){
+       echo "La palabra tiene todas las vocales";
+   }
+else {
+    echo "La palabra tiene vocales repetidas";
+}
+
+echo "<br>";  
+echo "Y la longitud de la plabra es: $longitud";
 ?>
